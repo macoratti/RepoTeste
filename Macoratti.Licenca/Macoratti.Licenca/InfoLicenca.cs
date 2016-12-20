9b97913cde3eea1c36410ad44b94eb63358908ba
macoratti <macoratti@MACORATTI>
2016-12-20 09:29:17 -0200

    incluindo a propriedade Data

--- Macoratti.Licenca/Macoratti.Licenca/InfoLicenca.cs
+++ Macoratti.Licenca/Macoratti.Licenca/InfoLicenca.cs
@@ -1,8 +1,12 @@
-﻿namespace Macoratti.Licenca
+﻿using System;
+
+namespace Macoratti.Licenca
 {
     public class InfoLicenca
     {
         public string Nome { get; set; }
         public string ChaveProduto { get; set; }
+
+        public DateTime Data { get; set; }
     }
 }
