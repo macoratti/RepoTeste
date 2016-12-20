13f4f53fa066db22ae2699f2e800eeea1b5e75ae
macoratti <macoratti@MACORATTI>
2016-12-20 08:35:41 -0200

    edit

--- Macoratti.Licenca/Macoratti.Licenca/TipoDeLicenca.cs
+++ Macoratti.Licenca/Macoratti.Licenca/TipoDeLicenca.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Macoratti.Licenca
+﻿namespace Macoratti.Licenca
 {
     public class TipoDeLicenca
     {
         public string TRIAL { get; set; }
-       
+        public string AVALIACAO { get; set; }
+        public string LIBERADA { get; set; }
     }
 }
